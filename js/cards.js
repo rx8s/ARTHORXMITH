ef95958557c80d055365e6cca6621b09236e94a0
ARTIT G <rtiix8@gmail.com>
2026-06-04 12:16:01 +0700

    additional

--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,1 @@
-
-export const cards=[
-{name:'Blue Slime',element:'Water',str:30,agi:20,vit:25,int:10,dex:15,luk:5},
-{name:'Fire Wolf',element:'Fire',str:35,agi:25,vit:20,int:15,dex:20,luk:8}
-];
+export const cards=[{id:'cat001',name:'Pastel Cat'}];
---
+++ js/game.js
@@ -0,0 +1,1 @@
+console.log('ARTHORXMITH MVP');
