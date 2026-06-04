795d8a679c083844d9996e726889ab3c5b20fdc1
ARTIT G <rtiix8@gmail.com>
2026-06-04 12:04:57 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,10 @@
-
-export const firebaseConfig={
- apiKey:"YOUR_API_KEY",
- authDomain:"YOUR_PROJECT.firebaseapp.com",
- projectId:"YOUR_PROJECT",
- storageBucket:"YOUR_PROJECT.appspot.com",
- messagingSenderId:"123456",
- appId:"YOUR_APP_ID"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyClywxcc1eGf4SnFUucDCcg3STL8CdXN0k",
+  authDomain: "arthorxmith.firebaseapp.com",
+  projectId: "arthorxmith",
+  storageBucket: "arthorxmith.firebasestorage.app",
+  messagingSenderId: "72714216236",
+  appId: "1:72714216236:web:b0b7690c40bffbdc7b0289",
+  measurementId: "G-VF91TFT8QV"
 };
